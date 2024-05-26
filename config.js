b2ad26b4e8f59579bfbee2c5f483ec7ac5e42bf3
Rahul srivastava <87309230+Rahulsrivastava0124@users.noreply.github.com>
2024-05-26 15:24:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const MONGO_DB = "mongodb://localhost:27017/"
+export const MONGO_DB = "mongodb+srv://AWSConnect:AWSMONGO@onmarket.10tbw3x.mongodb.net/?retryWrites=true&w=majority&appName=Onmarket"
 export const JWT_SECRET="rahulOnMarket"
